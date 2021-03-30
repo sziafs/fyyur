cbcff7c32cbbffe0256d4afc2bdd3b8509e576ad
Felipe <eng.fe.silveira@gmail.com>
2021-03-30 14:59:59 -0400

    fix: adding artist model missing fields

--- models.py
+++ models.py
@@ -26,9 +26,12 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String)
+    shows = db.relationship('Show', cascade='all, delete-orphan', backref='artist', lazy=True)
 
 
 class Show(db.Model):
