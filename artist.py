7731c6f96ce90be673d786b324da1f419b8a2965
Felipe <eng.fe.silveira@gmail.com>
2021-03-29 23:47:18 -0400

    feat: searching artists implemented

--- artist.py
+++ artist.py
@@ -13,18 +13,14 @@ def artists():
   return render_template('pages/artists.html', artists=data)
 
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    search_term = request.form.get('search_term', '')
+    response = Artist.query.filter(Artist.name.ilike('%' + search_term + '%'))
+    results = {
+        "count": response.count(),
+        "data": response
+    }
+
+    return render_template('pages/search_artists.html', results=results, search_term=search_term)
 
 def show_artist(artist_id):
   # shows the artist page with the given artist_id
