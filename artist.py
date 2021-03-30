8365fba170505824c11a635c6ba2cb742a74b050
Felipe <eng.fe.silveira@gmail.com>
2021-03-30 11:40:03 -0400

    feat: editing venueimplemented

--- artist.py
+++ artist.py
@@ -8,7 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def artists():
-  # TODO: replace with real data returned from querying the database
   data = Artist.query.all()
   return render_template('pages/artists.html', artists=data)
 
